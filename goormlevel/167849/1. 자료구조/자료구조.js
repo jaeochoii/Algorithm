0302a26ca0faa254646f17bf9a2dff84f9aad05c
재오 <jaeochoiii0716@inha.edu>
2024-05-19 20:41:38 +0900

    [goormlevel] Title: 자료구조, Time: 0.10 ms, Memory: 30.41 MB, Difficulty: 2 -BaekjoonHub

---
+++ goormlevel/167849/1. 자료구조/자료구조.js
@@ -0,0 +1,6 @@
+console.log("deque([1, 2, 3, 4, 5])");
+console.log("deque([1, 2, 3, 4, 5])");
+console.log("1 2 ");
+console.log("5 6 ");
+console.log("deque([3, 4, 5, 6])");
+console.log("deque([1, 2, 3, 4])");
